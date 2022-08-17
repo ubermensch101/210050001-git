f15846777c453427940bb50b3d10091e31afbbce
ubermensch101 <aaryandangi32@gmail.com>
2022-08-17 23:26:46 +0530

    Add polynomial rolling hash function

--- hashing.cpp
+++ hashing.cpp
@@ -1,13 +1,17 @@
 #include "hashing.hpp"
 
 int hash_string(string s) {
+	int ig=1;
 	int m=3;
+	int p=101;
+	int cur_p_val=1;
 	int l=s.length();
 	char c;
 	int hash_val=0;
 	for(int i=0; i<l; i++) {
 		c=s[i];
-		hash_val=(hash_val+int(c))%m;
+		hash_val=(hash_val+int(c)*cur_p_val)%m;
+		cur_p_val*=p;
 	}
 
 	return hash_val;
--- hashing.hpp
+++ hashing.hpp
@@ -6,20 +6,23 @@
 
 using namespace std;
 
+
 int hash_string(string s);
 
 int hash_string(string s) {
 	int m=3;
+	int p=101;
+	int cur_p_val=1;
 	int l=s.length();
 	char c;
 	int hash_val=0;
 	for(int i=0; i<l; i++) {
 		c=s[i];
-		hash_val=(hash_val+int(c))%m;
+		hash_val=(hash_val+int(c)*cur_p_val)%m;
+		cur_p_val*=p;
 	}
 
 	return hash_val;
 	
 }
-
 #endif
